01ce81d3430597cd6162cd61efea77a9a98a6862
Achieve-software <vtcbcdtyr@gmail.com>
2023-07-06 14:29:11 +0300

    about-finish

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="footer">
+      <div className="container">
+        <nav className="d-flex justify-content-center">
+          <NavLink
+            to="/"
+            className={(navData) =>
+              navData.isActive ? "nav-link active" : "nav-link"
+            }
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="about"
+            className={(navData) =>
+              navData.isActive ? "nav-link active" : "nav-link"
+            }
+          >
+            About
+          </NavLink>
+          <NavLink
+            to="products"
+            className={(navData) =>
+              navData.isActive ? "nav-link active" : "nav-link"
+            }
+          >
+            Products
+          </NavLink>
+        </nav>
+        <div className="copyrights">
+          &copy;{currentYear} <Link to="/"> WebStylePress </Link> -All Rights Reserved
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
