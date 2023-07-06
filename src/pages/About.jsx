ed08b3ff39859ddde63b4250247c9f56a5516a88
Achieve-software <vtcbcdtyr@gmail.com>
2023-07-06 13:39:33 +0300

    about-finish

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,41 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <main>
-      <div>
+    <main className="about">
+      <div className="pg-header">
+        <div className="container">
+          <h1>About</h1>
+        </div>
+      </div>
+      <div className="container content">
+        <div className="row">
+          <div className="col-lg-4">
+            <h2>Why Choose us.</h2>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia,
+              praesentium!
+            </p>
+          </div>
+          <div className="col-lg-4">
+            <h2>What we do</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam,
+              et?
+            </p>
+          </div>
+          <div className="col-lg-4">
+            <h2>Our Mission</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam,
+              et?
+            </p>
+          </div>
         
+        </div>
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default About
+export default About;
