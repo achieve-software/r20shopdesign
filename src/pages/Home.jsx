d01cc4d23522b13a5ddcd98a2f1f3123308e9017
Achieve-software <vtcbcdtyr@gmail.com>
2023-07-06 13:25:09 +0300

    about

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <main>
+      <div className="pg-header">
+        <div className="container">
+          <h1>Welcome To WebstylePress </h1>
+        </div>
+      </div>
+      <div className="container content">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+          placeat nesciunt dicta odit, quas asperiores voluptate ad mollitia
+          fugit minus animi molestiae quibusdam facere itaque repellat eos quo!
+          Adipisci, illo!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque officia
+          quae deserunt repellat nostrum mollitia fuga saepe, ullam doloremque
+          perspiciatis est officiis libero earum neque enim velit magni optio
+          accusamus vitae possimus eum, soluta, eveniet sit. Perspiciatis dolor
+          ex cum.
+        </p>
+      </div>
+    </main>
+  );
+};
 
-export default Home
+export default Home;
