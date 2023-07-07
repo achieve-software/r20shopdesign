eb4c0c7180f4bab45ff6f2f826af4a1973cbdbfd
Achieve-software <vtcbcdtyr@gmail.com>
2023-07-07 16:31:53 +0300

    last

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import './App.css';
 import Error from "./pages/Error";
 import About from "./pages/About";
 import Products from "./pages/Products";
+import SingleProduct from "./pages/SingleProduct";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -19,6 +21,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/products" element={<Products />} />
+          <Route path="/products/:productId" element={<SingleProduct/>} />
           <Route path="*" element={<Error />} />
         </Routes>
         <Footer />
